65e7a704f38894ce6d15967542a68511cfc7fe2e
JUAN ARECES <arecesj@gmail.com>
2018-11-07 16:22:44 -0800

    added API calls to /companies

--- frontend/src/CompanyList.js
+++ frontend/src/CompanyList.js
@@ -4,12 +4,19 @@ import CompanyCard from './CompanyCard';
 
 class CompanyList extends Component {
   render() {
+    const companies = <li>'umm, reee?'</li>;
     return (
       <div className="CompanyList">
         <h1>Jobly! Company Listings</h1>
-        <h3>All the jobs in one, convenient place.</h3>
-        <h2>Welcome back!</h2>
-        <CompanyCard />
+        <div className="Search">
+          <form action="">
+            <input type="text" />
+            <button>Search</button>
+          </form>
+        </div>
+        <div className="Companies">
+          <ul>{companies}</ul>
+        </div>
       </div>
     );
   }
