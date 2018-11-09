c5f7bb197a6277625beaf4ece486c63cbb931375
JUAN ARECES <arecesj@gmail.com>
2018-11-09 10:38:29 -0800

    error handling for signup / login

--- frontend/src/LoginForm.js
+++ frontend/src/LoginForm.js
@@ -2,13 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class LoginForm extends Component {
-  static defaultProps = {
-    username: 'Username:',
-    password: 'Password:',
-    firstName: 'First Name:',
-    lastName: 'Last Name:',
-    email: 'Email:'
-  };
   constructor(props) {
     super(props);
     this.state = {
@@ -17,11 +10,11 @@ class LoginForm extends Component {
       firstName: '',
       lastName: '',
       email: '',
-      isLogin: true
+      isLogin: true,
+      errors: []
     };
     this.handleChange = this.handleChange.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
-    this.handleSignup = this.handleSignup.bind(this);
     this.handleCreate = this.handleCreate.bind(this);
     this.toggleToLogin = this.toggleToLogin.bind(this);
     this.toggleToSignup = this.toggleToSignup.bind(this);
@@ -33,32 +26,16 @@ class LoginForm extends Component {
 
   async handleLogin(evt) {
     evt.preventDefault();
-    await this.props.loginUser({
-      username: this.state.username,
-      password: this.state.password
-    });
-    this.setState({ username: '', password: '' });
-    this.props.history.push('/jobs');
-  }
-
-  async handleSignup(evt) {
-    evt.preventDefault();
-    await this.props.createUser({
-      username: this.state.username,
-      password: this.state.password,
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email
-    });
-    this.setState({
-      username: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      isLogin: true
-    });
-    this.props.history.push('/jobs');
+    try {
+      await this.props.loginUser({
+        username: this.state.username,
+        password: this.state.password
+      });
+      this.setState({ username: '', password: '' });
+      this.props.history.push('/jobs');
+    } catch (err) {
+      this.setState({ errors: err });
+    }
   }
 
   toggleToLogin() {
@@ -73,115 +50,122 @@ class LoginForm extends Component {
 
   async handleCreate(evt) {
     evt.preventDefault();
-    await this.props.createUser({
-      username: this.state.username,
-      password: this.state.password,
-      first_name: this.state.firstName,
-      last_name: this.state.lastName,
-      email: this.state.email
-    });
-    this.setState({
-      username: '',
-      password: '',
-      firstName: '',
-      lastName: '',
-      email: ''
-    });
-    this.props.history.push('/jobs');
+    try {
+      await this.props.createUser({
+        username: this.state.username,
+        password: this.state.password,
+        first_name: this.state.firstName,
+        last_name: this.state.lastName,
+        email: this.state.email
+      });
+      this.setState({
+        username: '',
+        password: '',
+        firstName: '',
+        lastName: '',
+        email: ''
+      });
+      this.props.history.push('/jobs');
+    } catch (err) {
+      this.setState({ errors: err });
+    }
   }
-
+  //TODO: Refactor forms to do the following:
+  // Ternary that decides whether or not onSubmit uses handleCreate or handleLogin
+  // Only have one form: two fields for handleLogin, display three addtnl field for handleCreate
   render() {
     let form;
     if (!this.state.isLogin) {
       form = (
-        <form className="Login" onSubmit={this.handleCreate}>
-          <label htmlFor="username">{this.props.username}</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="password">{this.props.password}</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="firstName">{this.props.firstName}</label>
-          <input
-            type="text"
-            id="firstName"
-            name="firstName"
-            value={this.state.firstName}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="lastName">{this.props.lastName}</label>
-          <input
-            type="text"
-            id="lastName"
-            name="lastName"
-            value={this.state.lastName}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="email">{this.props.email}</label>
-          <input
-            type="text"
-            id="email"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleChange}
-          />
-          <br />
-          <button>Submit</button>
-        </form>
+        <div>
+          <h1>SIGNUP</h1>
+          <form className="Login" onSubmit={this.handleCreate}>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="firstName">First Name: </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="lastName">Last Name: </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="email">Email: </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+            <br />
+            <button>Submit</button>
+          </form>
+        </div>
       );
     } else {
       form = (
-        <form className="Login" onSubmit={this.handleLogin}>
-          <label htmlFor="username">{this.props.username}</label>
-          <input
-            type="text"
-            id="username"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-          />
-          <br />
-          <label htmlFor="password">{this.props.password}</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-          />
-          <br />
-          <button>Submit</button>
-        </form>
+        <div>
+          <h1>LOGIN</h1>
+          <form className="Login" onSubmit={this.handleLogin}>
+            <label htmlFor="username">Username: </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleChange}
+            />
+            <br />
+            <label htmlFor="password">Password: </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+            <br />
+            <button>Submit</button>
+          </form>
+        </div>
       );
     }
 
-    /*
-    what is the best way to toggle login/sign up forms?
-    -load login form by default, have option for sign up
-    */
-
-    // let returningUser = this.state.isLogIn ? ... : ...
+    const errors = this.state.errors.map(e => <li>{e}</li>);
 
     return (
       <div>
-        <h1>LOGIN OR SIGNUP, THUG</h1>
         <button onClick={this.toggleToLogin}>Login</button>
         <button onClick={this.toggleToSignup}>Sign up</button>
         {form}
+        <ul>{errors}</ul>
       </div>
     );
   }
